166d694425508c7da4a21be38cd69c021728f5aa
sweet1125 <sweetygupta4605@gmail.com>
2020-11-18 15:15:09 +0530

    Revert "main menu after adding linux module"
    This reverts commit 0f59f2b9678cb382c53318f13c0fd7b373453910.

--- main_menu.py
+++ main_menu.py
@@ -1,13 +1,15 @@
+
 import os
 import getpass
 import menu_awscli as aws
 import hadoop as hp
-import linux as lx
+import docker as dk
 
 
 def login():
+	os.system('clear')
 	os.system("tput setaf 6")
-	print("\t\t-------Login to your TUI application-------\n\n")
+	print("\n\n\t\t-------Login to your TUI application-------\n\n")
 	os.system("tput setaf 7")
 	app_Pass = getpass.getpass("Enter password to start application: ")
 	return app_Pass
@@ -19,29 +21,30 @@ while True:
 
 		while True:
 			os.system("tput setaf 3")
-			print("\t\t\t--------------------------\n\t\t\t   Welcome to TUI Automated menu \n\t\t\t--------------------------")
+			print("\t\t\t-----------------------------\n\t\t\tWelcome to TUI Automated menu \n\t\t\t-----------------------------")
 								
 
 			os.system("tput setaf 6")
 			print("""
 				\n
-				Press 1 :  Linux
+				Press 1 :  RHEL 8 O.S. 
 				Press 2 :  AWS 
 				Press 3 :  Hadoop
 				Press 4 :  Docker
+
 				Press 0 :  Exit
 				""" )
 			os.system("tput setaf 7")
 			main = input("Enter your choice: ")
 
 			if int(main) == 1:
-				lx.control_menu()
+				print('Linux')
 			elif int(main) == 2:
 				aws.aws_login()
 			elif int(main)==3:
 				hp.menu()
 			elif int(main)==4:
-				print('docker')
+				dk.local()
 			elif int(main)==0:
 				exit()
 			else:
@@ -58,3 +61,7 @@ while True:
 			continue
 		else:
 			exit()
+
+	
+	
+	
